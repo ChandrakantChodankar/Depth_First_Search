5b72cbee41c3bd25daee8217bb35530fbeaba813
Chandrakant Chodankar <99192690+ChandrakantChodankar@users.noreply.github.com>
2023-04-22 18:50:51 +0530

    Add files via upload

---
+++ expt2-3.py
@@ -0,0 +1,35 @@
+# shortest path
+tree = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G'],
+    'D': [],
+    'E': [],
+    'F': [],
+    'G': []
+}
+
+start = input("Enter the start state: ")
+goal = input("Enter the goal state: ")
+
+
+def dfs_sp(tree):
+    visited = []
+    stack = [[start]]
+    if start == goal:
+        print("start is the goal node")
+    while stack:
+        path = stack.pop()
+        node = path[-1]
+        if node not in visited:
+            visited.append(node)
+        neighour = tree[node]
+        for i in neighour:
+            new_path = list(path)
+            new_path.append(i)
+            stack.append(new_path)
+            if i == goal:
+                return new_path
+
+
+print("path is ", dfs_sp(tree))
